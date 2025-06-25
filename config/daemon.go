d365cd3978a6bae3519261e14a451929b652d896
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-25 18:33:44 +0530

    Updated for max connection pool

--- config/daemon.go
+++ config/daemon.go
@@ -4,6 +4,7 @@ package config
 type DConfig struct {
 	DebugMode 	bool		`yaml:"debug_mode,omitempty"`   
 	SocketPath  string		`yaml:"socket_path,omitempty"`
+	MaxConnPool int 		`yaml:"max_conn_pool,omitempty"`	
 }
 
 /* normalization function */
@@ -17,6 +18,12 @@ func (d *DConfig) Normalize() error {
 	/* if SocketPath is empty, use "/var/run/laclm-daemon.sock" as default */
 	if d.SocketPath == "" {
 		d.SocketPath = "/var/run/laclm-daemon.sock"
+	}
+
+	/* if maximum connection pool is not set or less than equal to 0, set it to 1 */
+	if d.MaxConnPool <= 0 {
+		/* perform single connection operation */
+		d.MaxConnPool = 1
 	}
 
 	return nil
